5324d928cf0c2431db5a8ac5e86ea7970068aeea
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2018-11-23 23:35:53 +0100

    :bear: implement bit counter, passing tests :green_heart:
    Case A: Bit Counter

--- lib/count/bits.rb
+++ lib/count/bits.rb
@@ -2,7 +2,18 @@ require "count/bits/version"
 
 module Count
   module Bits
-    def perform
+    CHUNKSIZE = 2048
+
+    def perform(path)
+      { '0' => 0, '1' => 0 }.tap do |stats|
+        Pathname.new(path).open.each(nil, CHUNKSIZE) do |chunk|
+          %w[0 1].each do |zero_or_one|
+            stats[zero_or_one] = chunk.b.unpack('C*').reduce(stats[zero_or_one]) do |sum, i|
+              sum + i.to_s(2).count(zero_or_one)
+            end
+          end
+        end
+      end
     end
   end
 end
--- spec/count/bits_spec.rb
+++ spec/count/bits_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe Count::Bits do
     let(:fixture) { fixture_folder.join('20181028-DSC07521-1333x750.jpg') }
 
     describe 'zero counting' do
-      subject { perform(fixture)[:zeros] }
-      it { is_expected.to be > 0 }
+      subject { perform(fixture)['0'] }
+      it { is_expected.to be == 1_103_884 }
     end
 
     describe 'one counting' do
-      subject { perform(fixture)[:ones] }
-      it { is_expected.to be > 0 }
+      subject { perform(fixture)['1'] }
+      it { is_expected.to be == 1_399_531 }
     end
   end
 end
